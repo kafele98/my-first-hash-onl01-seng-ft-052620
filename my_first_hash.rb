e406de8744088d4a626cadfadb64968b5a6c4676
Zoe Keller <keller.zoe98@gmail.com>
2020-06-09 14:39:53 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,9 +23,9 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest["musket"]= "2"
+  shipping_manifest["musket"]= 2
   
-  shipping_manifest["gun powder"]= "4"
+  shipping_manifest["gun powder"]= 4
 
 end
 
